d46235b6b7048bdba278451ce5617d4155aa831d
Blaine Price <william.price@homes.com>
2015-02-12 10:53:44 -0500

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lendMeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lendMeApp
+ */
+angular.module('lendMeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
